eadb3b19170de5cd9ba31e540d77ddb0f2f91465
VoidTheAngel <69voidtheangel@gmail.com>
2026-09-17 05:59:22 -0400

    Fix Windows backend package to avoid Linux imports

--- src/solsrng_core/antiafk/backends/__init__.py
+++ src/solsrng_core/antiafk/backends/__init__.py
@@ -1,12 +1,15 @@
+"""Windows-only Anti-AFK input backends.
+
+Linux backends are intentionally not imported here. Importing them from this
+package caused PyInstaller/Windows startup to load xdotool/ydotool modules,
+which do not belong in the Windows build.
+"""
+
 from .base import InputBackend, InputBackendError
 from .windows import WindowsInputBackend
-from .xdotool import XdotoolBackend
-from .ydotool import YdotoolBackend
 
 __all__ = [
     "InputBackend",
     "InputBackendError",
     "WindowsInputBackend",
-    "XdotoolBackend",
-    "YdotoolBackend",
 ]
